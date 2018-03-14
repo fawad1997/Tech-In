48336bd91f02c8be684a90420e2bd4f58cdad048
fawad1997 <fawad_12@outlook.com>
2018-03-14 19:00:34 +0500

    master page set

---
+++ Tech-In/Models/City.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tech_In.Models
+{
+    public class City
+    {
+        [Key]
+        public int CityID { get; set; }
+        public string CityName { get; set; }
+        public int CountryID { get; set; }
+        public Country Country { get; set; }
+    }
+}
---
+++ Tech-In/Models/Country.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tech_In.Models
+{
+    public class Country
+    {
+        public int CountryID { get; set; }
+        public string CountryName { get; set; }
+        public string CountryCode { get; set; }
+        public string CountryPhoneCode { get; set; }
+        
+    }
+}
