b8090ee414f25cb4082f525346d47eb4767bd039
fawad1997 <fawad_12@outlook.com>
2018-03-18 01:05:36 +0500

    email verification

--- Tech-In/Startup.cs
+++ Tech-In/Startup.cs
@@ -29,7 +29,10 @@ namespace Tech_In
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
-            services.AddIdentity<ApplicationUser, IdentityRole>()
+            services.AddIdentity<ApplicationUser, IdentityRole>(config=>
+            {
+                config.SignIn.RequireConfirmedEmail = true;
+            })
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
 
@@ -37,6 +40,8 @@ namespace Tech_In
             services.AddTransient<IEmailSender, EmailSender>();
 
             services.AddMvc();
+
+            services.Configure<AuthMessageSenderOptions>(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
