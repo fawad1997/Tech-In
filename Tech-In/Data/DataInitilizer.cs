24987ffb21b31015f7170097dacc87532ec5e014
tayyabxatti <tayyabmushtaq2@gmail.com>
2018-05-27 16:41:39 +0500

    Data Initilizer in Data package

---
+++ Tech-In/Data/DataInitilizer.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tech_In.Data;
+
+namespace Tech_In.Models.Database
+{
+    public class DataInitilizer
+    {
+        public DataInitilizer()
+        {
+            using (var initObject = new ApplicationDbContext())
+            {
+                Country country = new Country()
+                {
+                    CountryID = 1,
+                    CountryCode = "46000",
+                    CountryName = "Pakistan",
+                    CountryPhoneCode = "+92"
+                };
+                City city = new City()
+                {
+                    CityID = 1,
+                    CityName = "Islamabad",
+                    Country = country,
+                    CountryID = 1
+                };
+                SkillTag skillTag = new SkillTag()
+                {
+                    SkillName = "ASP.NET",
+                    AddedByUserId = "1",
+                    SkillTagID = 1,
+                    ApprovedStatus = true,
+                    TimeApproved = DateTime.Now
+                };
+                UserQuestion userQuestion = new UserQuestion()
+                {
+                    UserQuestionID = 1,
+                    Title="I can't get the database connection!",
+                    Description="Where should i get my database connection in MySql",
+                    PostTime=DateTime.Now,
+                };
+                QuestionSkill questionSkill = new QuestionSkill()
+                {
+                    QuestionSkillID = 1,
+                    Tag = skillTag,
+                    Question = userQuestion
+                };
+                UserCertification userCertification = new UserCertification()
+                {
+                    UserCertificationID = 1,
+                    Name = "Microsoft Certified Developer",
+                    URL = "www.xyz.com",
+                    CertificationDate = DateTime.Now,
+                    ExpirationDate=DateTime.Now,
+                    LiscenceNo="A15D7EADS225"
+                    
+                };
+                
+            }
+        }
+    }
+}
